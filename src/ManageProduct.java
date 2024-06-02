c6c97ef14dc6e6bfedb3c12edfcb28fd4a52cb60
chanuthdias <150771629+chanuthdias@users.noreply.github.com>
2024-06-02 17:22:36 +0530

    created update and remove function

--- src/ManageProduct.java
+++ src/ManageProduct.java
@@ -72,13 +72,212 @@ public class ManageProduct {
             System.out.println();
         }
     }
-    public void updateProduct(){
+    public void updateProduct() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter product ID to update: ");
+        String productID = input.next();
+        String newProductID = "";
+        String productName = "";
+        String productDescription = "";
+        double productPurchasePrice = 0.0;
+        double productSellingPrice = 0.0;
+        int productQuantity = 0;
+        String updateQuery = null;
 
+        while (true) {
+            System.out.println("\nChange Menu");
+            System.out.println("1. Product ID");
+            System.out.println("2. Product Name");
+            System.out.println("3. description");
+            System.out.println("4. purchase price");
+            System.out.println("5. selling price");
+            System.out.println("6. quantity");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter new product ID: ");
+                    newProductID = input.next();
+                    updateQuery = "UPDATE Product SET productId = ? WHERE productId = ?";
+                    try (Connection conn = DatabaseConnector.getConnection();
+                         PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
+
+                        pstmt.setString(1, newProductID);
+                        pstmt.setString(2, productID);
+                        int rowsAffected = pstmt.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Product updated successfully.");
+                        } else {
+                            System.out.println("Product not found.");
+                        }
+                    } catch (SQLException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter new product name: ");
+                    productName = input.next();
+                    updateQuery = "UPDATE Product SET productName = ? WHERE productId = ?";
+                    try (Connection conn = DatabaseConnector.getConnection();
+                        PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
+
+                        pstmt.setString(1, productName);
+                        pstmt.setString(2, productID);
+                        int rowsAffected = pstmt.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Product updated successfully.");
+                        } else {
+                            System.out.println("Product not found.");
+                        }
+                    } catch (SQLException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter new description: ");
+                    productDescription = input.next();
+                    updateQuery = "UPDATE Product SET description = ? WHERE productId = ?";
+                    try (Connection conn = DatabaseConnector.getConnection();
+                         PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
+
+                        pstmt.setString(1, productDescription);
+                        pstmt.setString(2, productID);
+                        int rowsAffected = pstmt.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Product updated successfully.");
+                        } else {
+                            System.out.println("Product not found.");
+                        }
+                    } catch (SQLException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter new purchase price: ");
+                    productPurchasePrice = input.nextDouble();
+                    updateQuery = "UPDATE Product SET purchasePrice = ? WHERE productId = ?";
+                    try (Connection conn = DatabaseConnector.getConnection();
+                         PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
+
+                        pstmt.setDouble(1, productPurchasePrice);
+                        pstmt.setString(2, productID);
+                        int rowsAffected = pstmt.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Product updated successfully.");
+                        } else {
+                            System.out.println("Product not found.");
+                        }
+                    } catch (SQLException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 5:
+                    System.out.println("Enter new selling price: ");
+                    productSellingPrice = input.nextDouble();
+                    updateQuery = "UPDATE Product SET sellingPrice = ? WHERE productId = ?";
+                    try (Connection conn = DatabaseConnector.getConnection();
+                         PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
+
+                        pstmt.setDouble(1, productSellingPrice);
+                        pstmt.setString(2, productID);
+                        int rowsAffected = pstmt.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Product updated successfully.");
+                        } else {
+                            System.out.println("Product not found.");
+                        }
+                    } catch (SQLException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 6:
+                    System.out.println("Enter new quantity: ");
+                    productQuantity = input.nextInt();
+                    updateQuery = "UPDATE Product SET quantity = ? WHERE productId = ?";
+                    try (Connection conn = DatabaseConnector.getConnection();
+                         PreparedStatement pstmt = conn.prepareStatement(updateQuery)) {
+
+                        pstmt.setInt(1, productQuantity);
+                        pstmt.setString(2, productID);
+                        int rowsAffected = pstmt.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Product updated successfully.");
+                        } else {
+                            System.out.println("Product not found.");
+                        }
+                    } catch (SQLException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 7:
+                    return;
+                default:
+                    System.out.println("Invalid choice\n");
+            }
+        }
     }
     public void removeProduct() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter product ID to remove: ");
+        String productID = input.next();
 
+        String deleteQuery = "DELETE FROM Product WHERE productId = ?";
+
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(deleteQuery)) {
+
+            pstmt.setString(1, productID);
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Product removed successfully.");
+            } else {
+                System.out.println("Product not found.");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-    //public Product searchProduct(String productId) {}
+    public Product searchProduct(String productId) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        String query = "SELECT * FROM Product WHERE productId = ?";
+        Product product = null;
+
+        try {
+            conn = DatabaseConnector.getConnection();
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, productId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                String productName = rs.getString("productName");
+                String description = rs.getString("description");
+                double purchasePrice = rs.getDouble("purchasePrice");
+                double sellingPrice = rs.getDouble("sellingPrice");
+                int quantity = rs.getInt("quantity");
+
+                product = new Product(productId, productName, description, purchasePrice, sellingPrice, quantity);
+            } else {
+                System.out.println("Product not found.");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return product;
+    }
     public ArrayList getAllProducts() {
         Connection con = null;
         Statement stmt = null;
